f2447658e96914da40a08309b07b0342f36b39c7
undertakerprog <gt6600502@gmail.com>
2025-03-14 22:41:34 +0300

    add one-thread server

--- Server/Src/Program.cs
+++ Server/Src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using ClientInfoLibrary;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -7,6 +8,7 @@ namespace Server.Src
     public class Program
     {
         private const int Port = 5000;
+        private static readonly ClientManager ClientManager = new();
 
         public static void Main()
         {
@@ -39,14 +41,71 @@ namespace Server.Src
             var tcpListener = new TcpListener(IPAddress.Any, Port);
             tcpListener.Start();
 
+            Console.WriteLine("TCP server started. Waiting for connections...");
+
+            var clients = new List<TcpClient>();
+
             while (true)
             {
-                var client = tcpListener.AcceptTcpClient();
-                var clientEndPoint = client.Client.RemoteEndPoint?.ToString();
-                var ipAddress = clientEndPoint!.Split(':')[0];
-                Console.WriteLine($"[TCP] Client connected from {ipAddress}");
+                if (tcpListener.Pending())
+                {
+                    var client = tcpListener.AcceptTcpClient();
+                    var clientEndPoint = client.Client.RemoteEndPoint?.ToString();
+                    var ipAddress = clientEndPoint!.Split(':')[0];
+                    Console.WriteLine($"[TCP] Client connected from {ipAddress}");
 
-                Task.Run(() => TcpClientHandler.HandleClient(client, ipAddress));
+                    clients.Add(client);
+
+                    var stream = client.GetStream();
+                    if (ClientManager.CanDownloadFile(ipAddress) && ClientManager.GetFileName(ipAddress) != "No File")
+                    {
+                        var fileName = ClientManager.GetFileName(ipAddress) + "\r\n";
+                        var fileNameBytes = Encoding.UTF8.GetBytes("RESUME " + fileName);
+                        stream.Write(fileNameBytes, 0, fileNameBytes.Length);
+                    }
+                    else
+                    {
+                        var skipMessage = "skip\r\n"u8.ToArray();
+                        stream.Write(skipMessage, 0, skipMessage.Length);
+                    }
+                }
+
+                for (var i = clients.Count - 1; i >= 0; i--)
+                {
+                    var client = clients[i];
+                    var stream = client.GetStream();
+
+                    if (!stream.DataAvailable) continue;
+                    try
+                    {
+                        var buffer = new byte[1024];
+                        var bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                        if (bytesRead == 0)
+                        {
+                            Console.WriteLine($"[TCP] Client {client.Client.RemoteEndPoint} disconnected.");
+                            clients.RemoveAt(i);
+                            client.Close();
+                            continue;
+                        }
+
+                        var request = Encoding.UTF8.GetString(buffer, 0, bytesRead).TrimEnd('\r', '\n');
+                        var clientIp = ((IPEndPoint)client.Client.RemoteEndPoint!).Address.ToString();
+                        Console.WriteLine($"[TCP - {clientIp}] Received command: {request}");
+
+                        var response = CommandProcessor.ProcessCommand(request, client, ClientManager);
+                        var responseBytes = Encoding.UTF8.GetBytes(response + "\r\n");
+                        stream.Write(responseBytes, 0, responseBytes.Length);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error handling client: {ex.Message}");
+                        clients.RemoveAt(i);
+                        client.Close();
+                    }
+                }
+
+                Thread.Sleep(10);
             }
         }
 
